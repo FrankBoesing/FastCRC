7cb20e641518ec2d2cd2831504b3731209b0eb1d
Shea Riley <sheariley@live.com>
2019-08-24 09:44:00 -0500

    Added support for ARM

--- FastCRC_tables.h
+++ FastCRC_tables.h
@@ -31,7 +31,7 @@
 #define FastCRC_tables
 #include "inttypes.h"
 
-#if !defined(__SAM3X8E__)
+#if !defined(__SAM3X8E__) && !defined(__arm__)
 #if defined(__AVR__ ) || defined(__IMXRT1052__) || defined(__IMXRT1062__) || defined(ARDUINO_ARCH_STM32F1)
 #include <avr/pgmspace.h>
 #else
